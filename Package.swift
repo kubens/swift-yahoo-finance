a08819f9499ee5ba54247686c32db3687932c9b9
Jakub Łaptaś <kontakt@kubens.pl>
2025-08-08 14:42:15 +0200

    chore(deps): add swift-http-types dependency

--- Package.swift
+++ Package.swift
@@ -12,11 +12,17 @@ let package = Package(
       targets: ["YahooFinance"]
     )
   ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-http-types", from: "1.4.0")
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "YahooFinance"
+      name: "YahooFinance",
+      dependencies: [
+        .product(name: "HTTPTypes", package: "swift-http-types")
+      ]
     ),
     .testTarget(
       name: "YahooFinanceTests",
